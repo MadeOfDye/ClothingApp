0f4e664c7d02facd85114919579007579c9759f6
TimoceDarius <115105281+TimoceDarius@users.noreply.github.com>
2025-09-17 19:30:39 +0300

    Created static files for asset photos

---
+++ ClothingStoreApp/ClothingStore.Shared/Constants/Domain.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothingStore.Shared.Constants
+{
+    public class Domain
+    {
+        public static readonly string LOCAL_DOMAIN = "https://localhost:7135";
+    }
+}
